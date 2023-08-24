4a1cc4905059b174f8227c7a27dbdc9b1631eff0
sagar <sagartiwary360.com>
2023-08-24 19:40:13 +0530

    backend completed

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 const express = require("express");
 const { connection } = require("./DB/db");
 const { uRouter } = require("./Route/userRouter");
+const cors=require("cors")
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 4500;
+app.use(cors())
 app.use(express.json());
 app.use("/api",uRouter)
 app.listen(port, async (req, res) => {
